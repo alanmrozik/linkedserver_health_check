44961d9eea1c76242d4c3fd4f3bc3c230dfd028a
alanmrozik <alan.mrozik@summ-it.pl>
2026-04-27 12:26:36 +0200

    poprawki

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # linkedserver_health_check
-SQL Server's linked server and kerberos related T-SQL scripts to check what might be wrong with their configuration.
+SQL Server's linked server and kerberos related T-SQL scripts to check what might be wrong with their configuration. First part of the file contains code to run on the original instance, the second part to run on the target server.
+
+# Prerequisites (for sure not minimum)
+1. xp_cmdshell enabled
+2. powershell's ActiveDirecotry module installed
+3. local admin account on the servers.
+4. sysadmin account
--- linkedserver_health_check.sql
+++ linkedserver_health_check.sql
@@ -1,9 +1,25 @@
 --linkedserver_kerberos_problem_check_bundle_pack
 
---query variable
-DECLARE @query VARCHAR(8000);
+DECLARE @query VARCHAR(8000); --query variable
+
+DECLARE @xp_cmdshell_enabled INT; --variable to check if xp_cmdshell is enabled
+SELECT @xp_cmdshell_enabled =
+CAST(value_in_use AS INT)
+FROM sys.configurations
+WHERE name = 'xp_cmdshell';
 
 --BELOW QUERIES RUN ON THE SOURCE SERVER
+
+--check your linked servers configuration
+SELECT	server_id,
+		name,
+		product,
+		provider,
+		data_source,
+		is_data_access_enabled, --must be 1
+		is_remote_login_enabled AS 'is_RPC_enabled', --must be 1
+		is_rpc_out_enabled --must be 1
+FROM sys.servers 
 
 --check engine service account and their spns
 DECLARE @EngineServiceAccount VARCHAR(256);
@@ -19,21 +35,20 @@ FROM sys.dm_server_services
 WHERE 
 	filename LIKE '%sqlservr.exe%'
 
---check spns
+--check spns for your engine account
 SET @query = 'setspn -l ' + @EngineServiceAccount;
 EXEC xp_cmdshell @query;
 
-
---check kerberos delegation
-SET @query = 'powershell "Import-Module ActiveDirectory; Get-ADUser '+@EngineServiceAccount+' -Properties TrustedForDelegation,TrustedToAuthForDelegation"'
-EXEC xp_cmdshell @query;
-
---check service port 
+--check service port of your engine account
 SELECT 
     local_tcp_port
 FROM sys.dm_exec_connections
 WHERE session_id = @@SPID;
 
+--check kerberos delegation
+SET @query = 'powershell "Import-Module ActiveDirectory; Get-ADUser '+@EngineServiceAccount+' -Properties TrustedForDelegation,TrustedToAuthForDelegation"'
+EXEC xp_cmdshell @query;
+PRINT ERROR_MESSAGE();
 
 --is tcp\ip enabled?
 DECLARE @tcpEnabled INT;
@@ -44,7 +59,23 @@ EXEC master.dbo.xp_instance_regread
     @tcpEnabled OUTPUT;
 SELECT 
 	'Is TCP\IP Enabled?',
-@tcpEnabled AS '1 or 0';
+    CASE
+        WHEN @tcpEnabled = 1 THEN 'YES'
+        WHEN @tcpEnabled = 0 THEN 'NO'
+        ELSE 'IDK'
+    END AS 'Answer'
+
+--check DTC settings
+--properly working for:
+--AuthenticationLevel               : NoAuth
+--InboundTransactionsEnabled        : True
+--OutboundTransactionsEnabled       : True
+--RemoteClientAccessEnabled         : True
+--RemoteAdministrationAccessEnabled : True
+--XATransactionsEnabled             : True
+--LUTransactionsEnabled             : True
+SET @query = 'powershell "Get-DtcNetworkSetting"'
+EXEC xp_cmdshell @query;
 
 --BELOW QUERIES RUN ON THE TARGET SERVER
 SELECT auth_scheme 
